f1912e9f7ebe60e33aa31944f73ef880f0eb1b3e
Túlius <tulius_sr@outlook.com>
2023-03-04 12:40:18 -0300

    Mudança do tipo de ScheduleDate para Instant

--- src/main/java/com/flexpag/agendarpagamento/config/TestConfig.java
+++ src/main/java/com/flexpag/agendarpagamento/config/TestConfig.java
@@ -1,10 +1,7 @@
 package com.flexpag.agendarpagamento.config;
 
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Arrays;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -47,20 +44,15 @@ public class TestConfig implements CommandLineRunner{
 		Payment pay1= new Payment(1L,"Bradesco",150.50,Instant.parse("2020-06-20T19:50:07Z"),user1);
 		Payment pay2= new Payment(2L,"Itaú",420.50,Instant.parse("2021-03-16T12:10:07Z"),user2);
 		
-		user1.getPayments(pay1);
-		user2.getPayments(pay2);
-		
-		LocalDate data01 = LocalDate.parse("2023-03-17");
-		Date date= Date.from(data01.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		Schedule schedule1= new Schedule(1L,ScheduleStatus.PAID,date, user1);
-		
-		data01 = LocalDate.parse("2023-02-17");
-		date= Date.from(data01.atStartOfDay(ZoneId.systemDefault()).toInstant());
-		Schedule schedule2= new Schedule(2L,ScheduleStatus.PENDING,date,user2);
+		Schedule schedule1= new Schedule(1L,ScheduleStatus.PAID,Instant.parse("2020-06-20T19:50:07Z"),
+				user1);
+		Schedule schedule2= new Schedule(2L,ScheduleStatus.PENDING,Instant.parse("2020-06-20T19:50:07Z"),
+				user2);
 		
 		userRepository.saveAll(Arrays.asList(user1,user2));
-		scheduleRepository.saveAll(Arrays.asList(schedule1,schedule2));
 		paymentRepository.saveAll(Arrays.asList(pay1,pay2));
+		scheduleRepository.saveAll(Arrays.asList(schedule1,schedule2));
+		
 	}
 	
 }
